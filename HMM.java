726f1f16bfca9ed17cfa961e8e0f8fa24d7017c4
Jess Kane <jlk238@cornell.edu>
2013-04-05 18:00:53 -0400

    Sentences now split up into documents and paragraphs, for both training and test data.

--- HMM.java
+++ HMM.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.HasWord;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.process.DocumentPreprocessor;
-import edu.stanford.nlp.util.CoreMap;
 
 public class HMM {
 
@@ -24,6 +22,14 @@ public class HMM {
 	private HashMap<String,HashMap<String, Double>> transitions = new HashMap<String,HashMap<String, Double>>();
 	/* Emissions object, still working on this. */
 	private Emission emissions= new Emission();
+	
+	private ArrayList<ArrayList<ArrayList<ArrayList<String>>>> documents = null;
+	private HashMap<String, String> sentiments = null;		// "full sentence":"sentiment"
+
+	private ArrayList<ArrayList<ArrayList<ArrayList<String>>>> testDocuments = null;
+	private HashMap<String, String> testSentiments = null;		// "full sentence":"sentiment"
+
+	
 	/* Stanford NLP modelling pipeline, used in annotation. */
 	protected StanfordCoreNLP pipeline;
 	/* Train data location. */
@@ -45,15 +51,14 @@ public class HMM {
 
 	/* Reads through the file and generates a sentiment bigram model, and updates
 	 *  emission feature counts */
-	private void readFile(String fileLoc) {
+	public ArrayList<ArrayList<ArrayList<ArrayList<String>>>> readFile(String fileLoc) {
 
 		// Lists of sentences and their pos's. If I did this right, ArrayList sentences should be constructed
 		// so that sentences.get(i) corresponds to sentiments.get(i). This might be useful for calculating
 		// emissions probabilities. If not, feel free to disregard.
+		ArrayList<ArrayList<ArrayList<String>>> paragraphs = new ArrayList<ArrayList<ArrayList<String>>>();
 		ArrayList<ArrayList<String>> sentences = new ArrayList<ArrayList<String>>();
-		ArrayList<ArrayList<String>> poss = new ArrayList<ArrayList<String>>();
-		ArrayList<String> sentiments = new ArrayList<String>();
-
+				
 		// Create StanfordCoreNLP object properties, with POS tagging and tokenization
 		Properties props;
 		props = new Properties();
@@ -64,13 +69,24 @@ public class HMM {
 		ArrayList<String> sentPos= null;
 		ArrayList<String> sentence= null;
 		BufferedReader br= null;
+		
 		try{
 			br= new BufferedReader(new FileReader(fileLoc));
 			while(true){
 				// Read current line, skipping over if a review header [xxxx/xx],
 				// and breaking the loop if the end has been reached
 				String line= br.readLine();
+				
 				while(line.length()==0 || line.charAt(line.length()-1) != '>') {
+					
+					// If it's [xxxx/xx], switch documents
+					if (line.charAt(line.length()-1) != '>') {
+						documents.add(paragraphs);
+						paragraphs = (ArrayList<ArrayList<ArrayList<String>>>) paragraphs.clone();
+						paragraphs.clear();
+						sentences = (ArrayList<ArrayList<String>>) sentences.clone();
+						sentences.clear();					
+					} 
 					line= br.readLine();
 					if(line==null) break;
 				}
@@ -86,19 +102,20 @@ public class HMM {
 				// Iterate over all of the tokens on a line
 				for (CoreLabel token: tokens) {
 					if (token.value().equals("{}")) {
-						// Do nothing
+						// {} denotes a new paragraph
+						paragraphs.add(sentences);
+						sentences = (ArrayList<ArrayList<String>>) sentences.clone();
+						sentences.clear();	
 					} else {
 						sentPos.add(token.tag());
 						sentence.add(token.value());
 					}
 				}
 				sentiment= sentence.get(sentence.size()-2);
-				sentiments.add(sentiment);
+				sentiments.put(line, sentiment);
 				//Update sentiment tables
 				emissions.updTable(sentiment, sentence, sentPos);
 				sentences.add(sentence);
-				poss.add(sentPos);
-
 			}
 			br.close();
 		} catch(FileNotFoundException f) {
@@ -124,7 +141,8 @@ public class HMM {
 				sub.put(next, count);
 				transitions.put(current, sub);
 			}
-		}		
+		}
+		return documents;		
 	}
 
 	/* Returns the count for the bigram sentiment1 sentiment2 */
@@ -150,5 +168,110 @@ public class HMM {
 		for (Double i:values)
 			sum = sum + i;
 		return sum;
+	}
+
+	/**
+	 * Parse the test data.
+	 */
+	public ArrayList<ArrayList<ArrayList<ArrayList<String>>>> parseTestData (String filename) {
+		// Lists of sentences and their pos's. If I did this right, ArrayList sentences should be constructed
+		// so that sentences.get(i) corresponds to sentiments.get(i). This might be useful for calculating
+		// emissions probabilities. If not, feel free to disregard.
+		ArrayList<ArrayList<ArrayList<String>>> paragraphs = new ArrayList<ArrayList<ArrayList<String>>>();
+		ArrayList<ArrayList<String>> sentences = new ArrayList<ArrayList<String>>();
+		
+		// ArrayLists to store the words and pos's on the current line
+		ArrayList<String> sentPos= null;
+		ArrayList<String> sentence= null;
+		
+		// Create StanfordCoreNLP object properties, with POS tagging
+		Properties props;
+		props = new Properties();
+		props.put("annotators", "tokenize, ssplit, pos");
+
+		// StanfordCoreNLP loads a lot of models, so you probably
+		// only want to do this once per execution
+		this.pipeline = new StanfordCoreNLP(props);
+		BufferedReader br = null;
+		PrintWriter out = null;
+
+		try {
+			br= new BufferedReader(new FileReader(filename));
+			out = new PrintWriter(new BufferedWriter(new FileWriter("./results.txt", true)));
+		} catch(FileNotFoundException e) {
+			System.out.println("That file doesn't exist. Try again.");
+		} catch(IOException d) {
+			System.out.println(d.getMessage());
+		}		
+
+		if (br != null && out != null) {
+			// Reads through the test file line by line
+			while(true) {
+				
+				// Read current line, skipping over if a review header [xxxx/xx],
+				// and breaking the loop if the end has been reached
+				String line = "";
+				try {
+					line = br.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				while(line.length()==0 || line.charAt(line.length()-1) != '>') {
+					
+					// If it's [xxxx/xx], switch documents
+					if (line.charAt(line.length()-1) != '>') {
+						testDocuments.add(paragraphs);
+						paragraphs = (ArrayList<ArrayList<ArrayList<String>>>) paragraphs.clone();
+						paragraphs.clear();
+						sentences = (ArrayList<ArrayList<String>>) sentences.clone();
+						sentences.clear();					
+					} 
+					
+					try {
+						line= br.readLine();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					if(line==null) break;
+				}
+				if(line==null) break;
+				
+				sentPos= new ArrayList<String>();
+				sentence= new ArrayList<String>();
+				//Sentiment value for current line
+				String sentiment= null;
+				// Stanford stuff
+				Annotation lineAnno= new Annotation(line);
+				this.pipeline.annotate(lineAnno);
+				List<CoreLabel> tokens = lineAnno.get(TokensAnnotation.class);
+				// Iterate over all of the tokens on a line
+				for (CoreLabel token: tokens) {
+					if (token.value().equals("{}")) {
+						// {} denotes a new paragraph
+						paragraphs.add(sentences);
+						sentences = (ArrayList<ArrayList<String>>) sentences.clone();
+						sentences.clear();	
+					} else {
+						sentPos.add(token.tag());
+						sentence.add(token.value());
+					}
+				}
+				sentiment= sentence.get(sentence.size()-2);
+				testSentiments.put(line, sentiment);
+				//Update sentiment tables
+				sentences.add(sentence);
+			}
+
+			//Close input/output stream
+			try {
+				br.close();
+				out.close();
+			} catch(FileNotFoundException e) {
+				System.out.println("That file doesn't exist. Try again.");
+			} catch(IOException d) {
+				System.out.println(d.getMessage());
+			}	
+		}
+		return testDocuments;
 	}
 }
