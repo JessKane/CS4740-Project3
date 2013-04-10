7bbbc85bdc517ea1c6c2b2720715aac53a3f870a
yl588 <yl588@cornell.edu>
2013-04-10 03:05:52 -0300

    Update KFold.java

--- KFold.java
+++ KFold.java
@@ -1,34 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class KFold {
-  
-	public static void main(String[] args) {
-		/*HMM original = new HMM("./train/ScottRenshaw_train.txt");
-		HashMap<ArrayList<String>, String> correctSentiments = original.getSentiments();
+	
+	public static void main(String[] args) throws IOException {
 		Double d = 0.0;
 		String trainBase = "./validation/ScottRenshaw_train";
-		String testBase = ".validation/ScottRenshaw_test";
+		String testBase = "./validation/ScottRenshaw_test";
 		for (int i = 1; i <= 5; i++) {
 			String trainFile = trainBase + i + ".txt";
 			String testFile = testBase + i + ".txt";
-			HMM model = new HMM(trainFile);
-			model.parseTestData(testFile);
-			HashMap<ArrayList<String>, String> predicted = model.getResults();
-			d += check(correctSentiments, predicted);
+			HMM model = new HMM(trainFile, testFile, "./validation/predictions.txt");
+			d += check(testFile, "./validation/predictions.txt");
 		}
-		System.out.println(d/5 + "% accuracy");*/
+		System.out.println(d/5 + "% accuracy");
 	}
 	
-	public static Double check(HashMap<ArrayList<String>, String> checkedAgainst, HashMap<ArrayList<String>, String> fileToCheck) {
+	public static Double check(String originalFile, String predictionsFile) throws IOException {
 		Double numCorrect = 0.0;
-		for (ArrayList<String> sentence : fileToCheck.keySet()) {
-			if (fileToCheck.get(sentence).equals(checkedAgainst.get(sentence))) {
-				numCorrect = numCorrect + 1.0;
+		BufferedReader original = new BufferedReader(new FileReader(originalFile));
+		BufferedReader predictions = new BufferedReader(new FileReader(predictionsFile));
+		Double numLines = 0.0;
+		while (true) {
+			String originalLine = original.readLine();
+			String prediction = predictions.readLine();
+			if (originalLine==null) break;
+			if (originalLine.length()!=0) {
+				int endBracket = originalLine.length()-1;
+				int startBracket = originalLine.lastIndexOf('<');
+				String correctSentiment = originalLine.substring(startBracket+1, endBracket);
+				numLines++;
+				if (correctSentiment.equals(prediction)) {
+					numCorrect += 1.0;
+				}
 			}
+			else {
+				// Do nothing
+			}
+			
 		}
-		return numCorrect / fileToCheck.size();
+		original.close();
+		predictions.close();
+
+		return numCorrect / numLines;
 	}
 
 }
