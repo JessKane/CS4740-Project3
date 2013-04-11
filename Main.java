6eca071934c63dacdecb3f134dc6c80d9b9ee030
Brian Wojcik <bmw75@cornell.edu>
2013-04-11 04:03:13 -0400

    making test

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		HMM schwartz = new HMM("data/DennisSchwartz_train.txt","data/DennisSchwartz_train.txt","data/DennisSchwartz_train_results.txt", "dennis");
-		System.out.println(KFold.check("data/DennisSchwartz_train.txt", "data/DennisSchwartz_train_results.txt"));
-		HMM renshaw = new HMM("data/ScottRenshaw_train.txt","data/ScottRenshaw_train.txt","data/ScottRenshaw_train_results.txt", "scott");
-		System.out.println(KFold.check("data/ScottRenshaw_train.txt", "data/ScottRenshaw_train_results.txt"));
+		//HMM schwartz = new HMM("data/DennisSchwartz_train.txt","data/DennisSchwartz_train.txt","data/DennisSchwartz_train_results.txt", "dennis");
+		//System.out.println(KFold.check("data/DennisSchwartz_train.txt", "data/DennisSchwartz_train_results.txt"));
+		//HMM renshaw = new HMM("data/ScottRenshaw_train.txt","data/ScottRenshaw_train.txt","data/ScottRenshaw_train_results.txt", "scott");
+		//System.out.println(KFold.check("data/ScottRenshaw_train.txt", "data/ScottRenshaw_train_results.txt"));
 		
-//		HMM test = new HMM("data/DennisSchwartz_train.txt","data/DennisSchwartz_test.txt","data/DennisSchwartz_test_results.txt", "dennis");
-//		HMM test2 = new HMM("data/ScottRenshaw_train.txt","data/ScottRenshaw_test.txt","data/ScottRenshaw_test_results.txt", "scott");
+		HMM test = new HMM("data/DennisSchwartz_train.txt","data/DennisSchwartz_test.txt","data/DennisSchwartz_test_results.txt", "dennis");
+		HMM test2 = new HMM("data/ScottRenshaw_train.txt","data/ScottRenshaw_test.txt","data/ScottRenshaw_test_results.txt", "scott");
 		
 		//HMM renshaw = new HMM("data/ScottRenshaw_train.txt","data/test.txt","data/test_results.txt");
 	}
